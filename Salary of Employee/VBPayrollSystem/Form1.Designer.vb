d59dfec6315a519da2352e81c6f27e1c4d8864ae
jerald12 <83905509+jcsmart12@users.noreply.github.com>
2024-02-11 23:29:38 +0800

    Add files via upload

---
+++ Salary of Employee/VBPayrollSystem/Form1.Designer.vb
@@ -0,0 +1,376 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.RATE = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.SSS = New System.Windows.Forms.TextBox()
+        Me.PAGIBIG = New System.Windows.Forms.TextBox()
+        Me.PHILHEALTH = New System.Windows.Forms.TextBox()
+        Me.TAXS = New System.Windows.Forms.TextBox()
+        Me.CALC = New System.Windows.Forms.Button()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.NETPAY = New System.Windows.Forms.TextBox()
+        Me.GROSSPAY = New System.Windows.Forms.TextBox()
+        Me.DAYS = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.DEDUCS = New System.Windows.Forms.TextBox()
+        Me.EXT = New System.Windows.Forms.Button()
+        Me.CLS = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(181, 32)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(109, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Rate / Day"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Imprint MT Shadow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(295, 14)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 24)
+        Me.Label2.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(490, 22)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(117, 46)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Number Of" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Days Work"
+        '
+        'RATE
+        '
+        Me.RATE.Location = New System.Drawing.Point(298, 35)
+        Me.RATE.Margin = New System.Windows.Forms.Padding(4)
+        Me.RATE.Name = "RATE"
+        Me.RATE.Size = New System.Drawing.Size(132, 22)
+        Me.RATE.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Imprint MT Shadow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(592, 68)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 24)
+        Me.Label4.TabIndex = 5
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Arial Rounded MT Bold", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(84, 85)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(305, 32)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Deductions Of Salary"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(121, 237)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(95, 23)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "10% TAX"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(121, 204)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(143, 23)
+        Me.Label7.TabIndex = 8
+        Me.Label7.Text = "PHIL HEALTH"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(121, 134)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(54, 23)
+        Me.Label8.TabIndex = 9
+        Me.Label8.Text = "SSS "
+        '
+        'SSS
+        '
+        Me.SSS.Location = New System.Drawing.Point(300, 134)
+        Me.SSS.Margin = New System.Windows.Forms.Padding(4)
+        Me.SSS.Name = "SSS"
+        Me.SSS.ReadOnly = True
+        Me.SSS.Size = New System.Drawing.Size(132, 22)
+        Me.SSS.TabIndex = 3
+        '
+        'PAGIBIG
+        '
+        Me.PAGIBIG.Location = New System.Drawing.Point(300, 171)
+        Me.PAGIBIG.Margin = New System.Windows.Forms.Padding(4)
+        Me.PAGIBIG.Name = "PAGIBIG"
+        Me.PAGIBIG.ReadOnly = True
+        Me.PAGIBIG.Size = New System.Drawing.Size(132, 22)
+        Me.PAGIBIG.TabIndex = 4
+        '
+        'PHILHEALTH
+        '
+        Me.PHILHEALTH.Location = New System.Drawing.Point(300, 206)
+        Me.PHILHEALTH.Margin = New System.Windows.Forms.Padding(4)
+        Me.PHILHEALTH.Name = "PHILHEALTH"
+        Me.PHILHEALTH.ReadOnly = True
+        Me.PHILHEALTH.Size = New System.Drawing.Size(132, 22)
+        Me.PHILHEALTH.TabIndex = 5
+        '
+        'TAXS
+        '
+        Me.TAXS.Location = New System.Drawing.Point(300, 239)
+        Me.TAXS.Margin = New System.Windows.Forms.Padding(4)
+        Me.TAXS.Name = "TAXS"
+        Me.TAXS.ReadOnly = True
+        Me.TAXS.Size = New System.Drawing.Size(132, 22)
+        Me.TAXS.TabIndex = 6
+        '
+        'CALC
+        '
+        Me.CALC.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.CALC.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.CALC.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CALC.Location = New System.Drawing.Point(466, 228)
+        Me.CALC.Margin = New System.Windows.Forms.Padding(4)
+        Me.CALC.Name = "CALC"
+        Me.CALC.Size = New System.Drawing.Size(117, 55)
+        Me.CALC.TabIndex = 10
+        Me.CALC.Text = "&CALCULATE"
+        Me.CALC.UseVisualStyleBackColor = False
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Arial Rounded MT Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(85, 274)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(207, 28)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "Total Deductions"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(497, 131)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(110, 23)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "Gross Pay"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Imprint MT Shadow", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(497, 187)
+        Me.Label11.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(0, 24)
+        Me.Label11.TabIndex = 19
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Arial Rounded MT Bold", 13.8!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(497, 171)
+        Me.Label12.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(101, 28)
+        Me.Label12.TabIndex = 20
+        Me.Label12.Text = "Net Pay"
+        '
+        'NETPAY
+        '
+        Me.NETPAY.Location = New System.Drawing.Point(615, 174)
+        Me.NETPAY.Margin = New System.Windows.Forms.Padding(4)
+        Me.NETPAY.Name = "NETPAY"
+        Me.NETPAY.ReadOnly = True
+        Me.NETPAY.Size = New System.Drawing.Size(132, 22)
+        Me.NETPAY.TabIndex = 9
+        '
+        'GROSSPAY
+        '
+        Me.GROSSPAY.Location = New System.Drawing.Point(615, 131)
+        Me.GROSSPAY.Margin = New System.Windows.Forms.Padding(4)
+        Me.GROSSPAY.Name = "GROSSPAY"
+        Me.GROSSPAY.ReadOnly = True
+        Me.GROSSPAY.Size = New System.Drawing.Size(132, 22)
+        Me.GROSSPAY.TabIndex = 8
+        '
+        'DAYS
+        '
+        Me.DAYS.Location = New System.Drawing.Point(615, 35)
+        Me.DAYS.Margin = New System.Windows.Forms.Padding(4)
+        Me.DAYS.Name = "DAYS"
+        Me.DAYS.Size = New System.Drawing.Size(132, 22)
+        Me.DAYS.TabIndex = 2
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.Location = New System.Drawing.Point(121, 169)
+        Me.Label13.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(102, 23)
+        Me.Label13.TabIndex = 24
+        Me.Label13.Text = "PAG-IBIG"
+        '
+        'DEDUCS
+        '
+        Me.DEDUCS.Location = New System.Drawing.Point(300, 276)
+        Me.DEDUCS.Margin = New System.Windows.Forms.Padding(4)
+        Me.DEDUCS.Name = "DEDUCS"
+        Me.DEDUCS.ReadOnly = True
+        Me.DEDUCS.Size = New System.Drawing.Size(132, 22)
+        Me.DEDUCS.TabIndex = 7
+        '
+        'EXT
+        '
+        Me.EXT.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.EXT.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.EXT.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.EXT.Location = New System.Drawing.Point(716, 228)
+        Me.EXT.Margin = New System.Windows.Forms.Padding(4)
+        Me.EXT.Name = "EXT"
+        Me.EXT.Size = New System.Drawing.Size(117, 55)
+        Me.EXT.TabIndex = 12
+        Me.EXT.Text = "&EXIT"
+        Me.EXT.UseVisualStyleBackColor = False
+        '
+        'CLS
+        '
+        Me.CLS.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.CLS.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.CLS.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CLS.Location = New System.Drawing.Point(591, 228)
+        Me.CLS.Margin = New System.Windows.Forms.Padding(4)
+        Me.CLS.Name = "CLS"
+        Me.CLS.Size = New System.Drawing.Size(117, 55)
+        Me.CLS.TabIndex = 11
+        Me.CLS.Text = "CLEA&R SCREEN"
+        Me.CLS.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Honeydew
+        Me.ClientSize = New System.Drawing.Size(843, 311)
+        Me.Controls.Add(Me.CLS)
+        Me.Controls.Add(Me.EXT)
+        Me.Controls.Add(Me.DEDUCS)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.GROSSPAY)
+        Me.Controls.Add(Me.NETPAY)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.CALC)
+        Me.Controls.Add(Me.TAXS)
+        Me.Controls.Add(Me.PHILHEALTH)
+        Me.Controls.Add(Me.PAGIBIG)
+        Me.Controls.Add(Me.SSS)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.DAYS)
+        Me.Controls.Add(Me.RATE)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximumSize = New System.Drawing.Size(861, 358)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Employee Salary"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents RATE As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents SSS As System.Windows.Forms.TextBox
+    Friend WithEvents PAGIBIG As System.Windows.Forms.TextBox
+    Friend WithEvents PHILHEALTH As System.Windows.Forms.TextBox
+    Friend WithEvents TAXS As System.Windows.Forms.TextBox
+    Friend WithEvents CALC As System.Windows.Forms.Button
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents NETPAY As System.Windows.Forms.TextBox
+    Friend WithEvents GROSSPAY As System.Windows.Forms.TextBox
+    Friend WithEvents DAYS As System.Windows.Forms.TextBox
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents DEDUCS As System.Windows.Forms.TextBox
+    Friend WithEvents EXT As System.Windows.Forms.Button
+    Friend WithEvents CLS As System.Windows.Forms.Button
+
+End Class
---
+++ Salary of Employee/VBPayrollSystem/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Dim s As Integer
+    Dim pg As Integer
+    Dim phil As Integer
+    Dim tax As Single
+    Dim deduc As Single
+    Dim gp As Single
+    Dim net As Single
+
+    Private Sub CALC_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CALC.Click
+        gp = (RATE.Text * DAYS.Text)
+        GROSSPAY.Text = gp.ToString("0.00")
+
+        s = 390
+        SSS.Text = s
+
+        pg = 200
+        PAGIBIG.Text = pg
+
+        phil = 350
+        PHILHEALTH.Text = phil
+
+        tax = gp * 0.1
+        TAXS.Text = tax
+
+        deduc = (s + pg + phil + tax)
+        DEDUCS.Text = deduc.ToString("0.00")
+
+        net = (GROSSPAY.Text - DEDUCS.Text)
+        NETPAY.Text = net.ToString("0.00")
+
+    End Sub
+
+    Private Sub EXT_Click(sender As Object, e As EventArgs) Handles EXT.Click
+        Me.Close()
+    End Sub
+
+    Private Sub CLS_Click(sender As Object, e As EventArgs) Handles CLS.Click
+        For Each Control As Control In Me.Controls
+            If TypeOf Control Is TextBox Then
+                Control.Text = String.Empty
+            End If
+        Next
+    End Sub
+End Class

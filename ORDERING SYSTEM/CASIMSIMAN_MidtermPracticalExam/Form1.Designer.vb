ca347492be35a142938a8da9b2ca800c49db52bc
jerald12 <83905509+jcsmart12@users.noreply.github.com>
2024-02-11 23:32:24 +0800

    Add files via upload

---
+++ ORDERING SYSTEM/CASIMSIMAN_MidtermPracticalExam/Form1.Designer.vb
@@ -0,0 +1,390 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MCRb1 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.OPRb10 = New System.Windows.Forms.RadioButton()
+        Me.OTRb9 = New System.Windows.Forms.RadioButton()
+        Me.MORb8 = New System.Windows.Forms.RadioButton()
+        Me.PSRb7 = New System.Windows.Forms.RadioButton()
+        Me.SWRb6 = New System.Windows.Forms.RadioButton()
+        Me.TTRb5 = New System.Windows.Forms.RadioButton()
+        Me.SARb4 = New System.Windows.Forms.RadioButton()
+        Me.HGRb3 = New System.Windows.Forms.RadioButton()
+        Me.FRRb2 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.ClrBtn = New System.Windows.Forms.Button()
+        Me.AddBtn = New System.Windows.Forms.Button()
+        Me.FiveDsc = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SubTotalLbl = New System.Windows.Forms.Label()
+        Me.VatLbl = New System.Windows.Forms.Label()
+        Me.DscLbl = New System.Windows.Forms.Label()
+        Me.TotalLbl = New System.Windows.Forms.Label()
+        Me.OrderTxt = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MCRb1
+        '
+        Me.MCRb1.AutoSize = True
+        Me.MCRb1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MCRb1.Location = New System.Drawing.Point(37, 41)
+        Me.MCRb1.Name = "MCRb1"
+        Me.MCRb1.Size = New System.Drawing.Size(281, 26)
+        Me.MCRb1.TabIndex = 1
+        Me.MCRb1.TabStop = True
+        Me.MCRb1.Text = "Motorcycle Cover - P518.00"
+        Me.MCRb1.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.OPRb10)
+        Me.GroupBox1.Controls.Add(Me.OTRb9)
+        Me.GroupBox1.Controls.Add(Me.MORb8)
+        Me.GroupBox1.Controls.Add(Me.PSRb7)
+        Me.GroupBox1.Controls.Add(Me.SWRb6)
+        Me.GroupBox1.Controls.Add(Me.TTRb5)
+        Me.GroupBox1.Controls.Add(Me.SARb4)
+        Me.GroupBox1.Controls.Add(Me.HGRb3)
+        Me.GroupBox1.Controls.Add(Me.FRRb2)
+        Me.GroupBox1.Controls.Add(Me.MCRb1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(26, 247)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(666, 247)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Motorcycle Products"
+        '
+        'OPRb10
+        '
+        Me.OPRb10.AutoSize = True
+        Me.OPRb10.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OPRb10.Location = New System.Drawing.Point(355, 201)
+        Me.OPRb10.Name = "OPRb10"
+        Me.OPRb10.Size = New System.Drawing.Size(204, 26)
+        Me.OPRb10.TabIndex = 10
+        Me.OPRb10.TabStop = True
+        Me.OPRb10.Text = "Oil Pump - P240.00"
+        Me.OPRb10.UseVisualStyleBackColor = True
+        '
+        'OTRb9
+        '
+        Me.OTRb9.AutoSize = True
+        Me.OTRb9.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OTRb9.Location = New System.Drawing.Point(355, 162)
+        Me.OTRb9.Name = "OTRb9"
+        Me.OTRb9.Size = New System.Drawing.Size(246, 26)
+        Me.OTRb9.TabIndex = 9
+        Me.OTRb9.TabStop = True
+        Me.OTRb9.Text = "Oil Treatment - P100.00"
+        Me.OTRb9.UseVisualStyleBackColor = True
+        '
+        'MORb8
+        '
+        Me.MORb8.AutoSize = True
+        Me.MORb8.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MORb8.Location = New System.Drawing.Point(355, 120)
+        Me.MORb8.Name = "MORb8"
+        Me.MORb8.Size = New System.Drawing.Size(252, 26)
+        Me.MORb8.TabIndex = 8
+        Me.MORb8.TabStop = True
+        Me.MORb8.Text = "Motorcycle Oil - P315.00"
+        Me.MORb8.UseVisualStyleBackColor = True
+        '
+        'PSRb7
+        '
+        Me.PSRb7.AutoSize = True
+        Me.PSRb7.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PSRb7.Location = New System.Drawing.Point(355, 83)
+        Me.PSRb7.Name = "PSRb7"
+        Me.PSRb7.Size = New System.Drawing.Size(231, 26)
+        Me.PSRb7.TabIndex = 7
+        Me.PSRb7.TabStop = True
+        Me.PSRb7.Text = "Piston Set - P1,763.00"
+        Me.PSRb7.UseVisualStyleBackColor = True
+        '
+        'SWRb6
+        '
+        Me.SWRb6.AutoSize = True
+        Me.SWRb6.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SWRb6.Location = New System.Drawing.Point(355, 41)
+        Me.SWRb6.Name = "SWRb6"
+        Me.SWRb6.Size = New System.Drawing.Size(242, 26)
+        Me.SWRb6.TabIndex = 6
+        Me.SWRb6.TabStop = True
+        Me.SWRb6.Text = "Sidewheels - P1,695.00"
+        Me.SWRb6.UseVisualStyleBackColor = True
+        '
+        'TTRb5
+        '
+        Me.TTRb5.AutoSize = True
+        Me.TTRb5.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TTRb5.Location = New System.Drawing.Point(37, 201)
+        Me.TTRb5.Name = "TTRb5"
+        Me.TTRb5.Size = New System.Drawing.Size(235, 26)
+        Me.TTRb5.TabIndex = 5
+        Me.TTRb5.TabStop = True
+        Me.TTRb5.Text = "Tricycle Tire - P800.00"
+        Me.TTRb5.UseVisualStyleBackColor = True
+        '
+        'SARb4
+        '
+        Me.SARb4.AutoSize = True
+        Me.SARb4.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SARb4.Location = New System.Drawing.Point(37, 162)
+        Me.SARb4.Name = "SARb4"
+        Me.SARb4.Size = New System.Drawing.Size(286, 26)
+        Me.SARb4.TabIndex = 4
+        Me.SARb4.TabStop = True
+        Me.SARb4.Text = "Shock Absorber - P1,500.00"
+        Me.SARb4.UseVisualStyleBackColor = True
+        '
+        'HGRb3
+        '
+        Me.HGRb3.AutoSize = True
+        Me.HGRb3.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.HGRb3.Location = New System.Drawing.Point(37, 120)
+        Me.HGRb3.Name = "HGRb3"
+        Me.HGRb3.Size = New System.Drawing.Size(230, 26)
+        Me.HGRb3.TabIndex = 3
+        Me.HGRb3.TabStop = True
+        Me.HGRb3.Text = "Handle Grip - P290.00"
+        Me.HGRb3.UseVisualStyleBackColor = True
+        '
+        'FRRb2
+        '
+        Me.FRRb2.AutoSize = True
+        Me.FRRb2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FRRb2.Location = New System.Drawing.Point(37, 83)
+        Me.FRRb2.Name = "FRRb2"
+        Me.FRRb2.Size = New System.Drawing.Size(233, 26)
+        Me.FRRb2.TabIndex = 2
+        Me.FRRb2.TabStop = True
+        Me.FRRb2.Text = "Flasher Relay - P31.00"
+        Me.FRRb2.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.ClrBtn)
+        Me.GroupBox2.Controls.Add(Me.AddBtn)
+        Me.GroupBox2.Controls.Add(Me.FiveDsc)
+        Me.GroupBox2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(26, 511)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(666, 82)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Actions"
+        '
+        'ClrBtn
+        '
+        Me.ClrBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ClrBtn.Location = New System.Drawing.Point(451, 30)
+        Me.ClrBtn.Name = "ClrBtn"
+        Me.ClrBtn.Size = New System.Drawing.Size(144, 36)
+        Me.ClrBtn.TabIndex = 2
+        Me.ClrBtn.Text = "Clear Order"
+        Me.ClrBtn.UseVisualStyleBackColor = False
+        '
+        'AddBtn
+        '
+        Me.AddBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.AddBtn.Location = New System.Drawing.Point(253, 30)
+        Me.AddBtn.Name = "AddBtn"
+        Me.AddBtn.Size = New System.Drawing.Size(144, 36)
+        Me.AddBtn.TabIndex = 1
+        Me.AddBtn.Text = " Add Order"
+        Me.AddBtn.UseVisualStyleBackColor = False
+        '
+        'FiveDsc
+        '
+        Me.FiveDsc.AutoSize = True
+        Me.FiveDsc.Location = New System.Drawing.Point(31, 33)
+        Me.FiveDsc.Name = "FiveDsc"
+        Me.FiveDsc.Size = New System.Drawing.Size(152, 27)
+        Me.FiveDsc.TabIndex = 0
+        Me.FiveDsc.Text = "5% Discount"
+        Me.FiveDsc.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(22, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(202, 23)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Summary of Orders"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(476, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(151, 28)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "SUBTOTAL:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial Rounded MT Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(504, 94)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(123, 28)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "12% VAT:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial Rounded MT Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(477, 141)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(150, 28)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "DISCOUNT:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Arial Rounded MT Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(527, 200)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(100, 28)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "TOTAL:"
+        '
+        'SubTotalLbl
+        '
+        Me.SubTotalLbl.AutoSize = True
+        Me.SubTotalLbl.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SubTotalLbl.Location = New System.Drawing.Point(628, 51)
+        Me.SubTotalLbl.Name = "SubTotalLbl"
+        Me.SubTotalLbl.Size = New System.Drawing.Size(52, 23)
+        Me.SubTotalLbl.TabIndex = 10
+        Me.SubTotalLbl.Text = "0.00"
+        '
+        'VatLbl
+        '
+        Me.VatLbl.AutoSize = True
+        Me.VatLbl.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.VatLbl.Location = New System.Drawing.Point(628, 99)
+        Me.VatLbl.Name = "VatLbl"
+        Me.VatLbl.Size = New System.Drawing.Size(52, 23)
+        Me.VatLbl.TabIndex = 11
+        Me.VatLbl.Text = "0.00"
+        '
+        'DscLbl
+        '
+        Me.DscLbl.AutoSize = True
+        Me.DscLbl.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DscLbl.Location = New System.Drawing.Point(628, 144)
+        Me.DscLbl.Name = "DscLbl"
+        Me.DscLbl.Size = New System.Drawing.Size(52, 23)
+        Me.DscLbl.TabIndex = 12
+        Me.DscLbl.Text = "0.00"
+        '
+        'TotalLbl
+        '
+        Me.TotalLbl.AutoSize = True
+        Me.TotalLbl.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TotalLbl.Location = New System.Drawing.Point(628, 200)
+        Me.TotalLbl.Name = "TotalLbl"
+        Me.TotalLbl.Size = New System.Drawing.Size(52, 23)
+        Me.TotalLbl.TabIndex = 13
+        Me.TotalLbl.Text = "0.00"
+        '
+        'OrderTxt
+        '
+        Me.OrderTxt.Location = New System.Drawing.Point(26, 35)
+        Me.OrderTxt.Multiline = True
+        Me.OrderTxt.Name = "OrderTxt"
+        Me.OrderTxt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.OrderTxt.Size = New System.Drawing.Size(432, 193)
+        Me.OrderTxt.TabIndex = 14
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Honeydew
+        Me.ClientSize = New System.Drawing.Size(739, 605)
+        Me.Controls.Add(Me.OrderTxt)
+        Me.Controls.Add(Me.TotalLbl)
+        Me.Controls.Add(Me.DscLbl)
+        Me.Controls.Add(Me.VatLbl)
+        Me.Controls.Add(Me.SubTotalLbl)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "San Marcelino Motorcycle Parts Ordering System"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MCRb1 As System.Windows.Forms.RadioButton
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents OPRb10 As System.Windows.Forms.RadioButton
+    Friend WithEvents OTRb9 As System.Windows.Forms.RadioButton
+    Friend WithEvents MORb8 As System.Windows.Forms.RadioButton
+    Friend WithEvents PSRb7 As System.Windows.Forms.RadioButton
+    Friend WithEvents SWRb6 As System.Windows.Forms.RadioButton
+    Friend WithEvents TTRb5 As System.Windows.Forms.RadioButton
+    Friend WithEvents SARb4 As System.Windows.Forms.RadioButton
+    Friend WithEvents HGRb3 As System.Windows.Forms.RadioButton
+    Friend WithEvents FRRb2 As System.Windows.Forms.RadioButton
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents ClrBtn As System.Windows.Forms.Button
+    Friend WithEvents AddBtn As System.Windows.Forms.Button
+    Friend WithEvents FiveDsc As System.Windows.Forms.CheckBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents SubTotalLbl As System.Windows.Forms.Label
+    Friend WithEvents VatLbl As System.Windows.Forms.Label
+    Friend WithEvents DscLbl As System.Windows.Forms.Label
+    Friend WithEvents TotalLbl As System.Windows.Forms.Label
+    Friend WithEvents OrderTxt As System.Windows.Forms.TextBox
+
+End Class
---
+++ ORDERING SYSTEM/CASIMSIMAN_MidtermPracticalExam/Form1.vb
@@ -0,0 +1,75 @@
+﻿Public Class Form1
+    Dim item As Double
+    Dim vat As Double
+    Dim subtotal As Double
+    Dim discount As Double
+    Dim total As Double
+
+    Private Sub AddBtn_Click(sender As Object, e As EventArgs) Handles AddBtn.Click
+        Dim itemName As String = " "
+
+        If MCRb1.Checked = True Then
+            item = 518.0
+            itemName = "Motor Cycle Cover - Php 518.00"
+        ElseIf FRRb2.Checked = True Then
+            item = 31.0
+            itemName = "Flasher Relay - Php 31.00"
+        ElseIf HGRb3.Checked = True Then
+            item = 290.0
+            itemName = "Handle Grip - Php 290.00"
+        ElseIf SARb4.Checked = True Then
+            item = 1500.0
+            itemName = "Shock Absorber - Php 1500.00"
+        ElseIf TTRb5.Checked = True Then
+            item = 800.0
+            itemName = "Tricycle Tire - Php 800.00"
+        ElseIf SWRb6.Checked = True Then
+            item = 1695.0
+            itemName = "Sidewheels - Php 1695.00"
+        ElseIf PSRb7.Checked = True Then
+            item = 1763.0
+            itemName = "Piston Set - Php 1763.00"
+        ElseIf MORb8.Checked = True Then
+            item = 315.0
+            itemName = "Motorcycle Oil - Php 315.00"
+        ElseIf OTRb9.Checked = True Then
+            item = 100.0
+            itemName = "Oil Treatment - Php 100.00"
+        ElseIf OPRb10.Checked = True Then
+            item = 240.0
+            itemName = "Oil Pump - Php 240.00"
+        End If
+
+        subtotal = subtotal + item
+        vat = subtotal * 0.12
+
+        If FiveDsc.Checked = True Then
+            discount = (subtotal + vat) * 0.05
+            total = (subtotal + vat) - discount
+        Else
+            total = subtotal + vat
+        End If
+
+        SubTotalLbl.Text = subtotal.ToString("0.00")
+        VatLbl.Text = vat.ToString("0.00")
+        DscLbl.Text = discount.ToString("0.00")
+        TotalLbl.Text = total.ToString("0.00")
+        OrderTxt.Text = OrderTxt.Text & itemName & ControlChars.NewLine
+
+    End Sub
+
+    Private Sub ClrBtn_Click(sender As Object, e As EventArgs) Handles ClrBtn.Click
+        subtotal = 0
+        vat = 0
+        discount = 0
+        total = 0
+
+        SubTotalLbl.Text = ("0.00")
+        VatLbl.Text = ("0.00")
+        DscLbl.Text = ("0.00")
+        TotalLbl.Text = ("0.00")
+
+        OrderTxt.Clear()
+    End Sub
+
+End Class
